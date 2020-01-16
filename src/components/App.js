eaa6905140fb2b4844d89da9ae94fd065560a891
Cassie <cassandra.sloan16@gmail.com>
2020-01-17 10:12:13 +1100

    spun up server and started index.js, app.js and styles directory

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+// import "./styles/styles.scss";
+
+class App extends Component {
+    render() {
+        return(
+            <h1>this is the app!</h1>
+        )
+    }
+}
+
+export default App;
