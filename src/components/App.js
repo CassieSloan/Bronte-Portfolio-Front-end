61487f773db7a9f5e377d5bbbf69a5b459806b66
Cassie <cassandra.sloan16@gmail.com>
2020-01-17 12:19:17 +1100

    added carousel to homepage and set up route for home/index page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React, {Component} from "react";
 import Header from "./shared/Header";
+import Home from "./pages/Home";
+import {BrowserRouter, Route, Link} from "react-router-dom";
+
 
 class App extends Component {
     render() {
         return(
             <>
                 <Header/>
+                <BrowserRouter>
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+                </BrowserRouter>
             </>
         )
     }
