61279e4fe457e4d2949dba45a56524ad266e122b
Cassie <cassandra.sloan16@gmail.com>
2020-01-17 11:30:39 +1100

    bones for shared header and navbar set up

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -8,15 +8,18 @@ class Header extends Component {
         return(
             <>
                 <h1 className="header">BRONTE DAVIS</h1>
-                <div>
-                    <BrowserRouter>
-                        <Link to="/">Home</Link>
-                        <Link to="/gallery">Gallery</Link>
-                        <Link to="/about">About</Link>
-                        <Link to="/contact">Contact</Link>
-                        <Link to="/blog">Blog</Link>
+                <div className="navbar">
+                    <div className="social-icons">
+                        <img src="src\images\twitter.png" alt="twitter" className="icon"/>
+                        <img src="src\images\instagram.png" alt="instagram" className="icon"/>
+                    </div>
+                    <BrowserRouter className="pages">
+                        <Link to="/" className="nav-link">Home</Link>
+                        <Link to="/gallery" className="nav-link">Gallery</Link>
+                        <Link to="/about" className="nav-link">About</Link>
+                        <Link to="/contact" className="nav-link">Contact</Link>
+                        <Link to="/blog" className="nav-link">Blog</Link>
                     </BrowserRouter>
-                    
                 </div>
             </>
         )
