dd4d4fab0290cd686d471cceb5b54ec21c74dc79
Cassie <cassandra.sloan16@gmail.com>
2020-01-20 12:11:04 +1100

    additional styling to navbar

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import {BrowserRouter, Link} from "react-router-dom";
+import twitter from "./../../images/twitter.png"
+import instagram from "./../../images/instagram.png"
 
 import "./../../styles/shared/header_and_navbar.scss";
 
@@ -10,15 +12,19 @@ class Header extends Component {
                 <h1 className="header">BRONTE DAVIS</h1>
                 <div className="navbar">
                     <div className="social-icons">
-                        <img src="src\images\twitter.png" alt="twitter" className="icon"/>
-                        <img src="src\images\instagram.png" alt="instagram" className="icon"/>
+                        <div className="twitter" styles={{ backgroundImage:`image(${twitter})` }}></div>
+                        <div className="instagram" styles={{ backgroundImage:`image(${instagram})` }}></div>
+                        {/* <img src="src\images\twitter.png" alt="twitter" className="icon"/>
+                        <img src="src\images\instagram.png" alt="instagram" className="icon"/> */}
                     </div>
-                    <BrowserRouter className="pages">
-                        <Link to="/" className="nav-link">Home</Link>
-                        <Link to="/gallery" className="nav-link">Gallery</Link>
-                        <Link to="/about" className="nav-link">About</Link>
-                        <Link to="/contact" className="nav-link">Contact</Link>
-                        <Link to="/blog" className="nav-link">Blog</Link>
+                    <BrowserRouter>
+                        <div className="pages">
+                            <Link to="/" className="nav-link">Home</Link>
+                            <Link to="/gallery" className="nav-link">Gallery</Link>
+                            <Link to="/about" className="nav-link">About</Link>
+                            <Link to="/contact" className="nav-link">Contact</Link>
+                            <Link to="/blog" className="nav-link">Blog</Link>
+                        </div>
                     </BrowserRouter>
                 </div>
             </>
