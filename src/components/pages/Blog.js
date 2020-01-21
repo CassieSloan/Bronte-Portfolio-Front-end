6dda0c38455ae9c22b61dce9da680edb537221d3
Cassie <cassandra.sloan16@gmail.com>
2020-01-21 12:17:22 +1100

    successfully rendered all response data as individual posts on blog page

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,24 +1,41 @@
-import React, {Component} from "react";
-import "./../../styles/blog.scss"
+import React, { Component } from "react";
+import axios from "axios";
+import "./../../styles/blog.scss";
 
-class App extends Component {
-    render() {
-        return(
-            <>
-                <div className="blog-post-container">
-                    <div className="image-container"></div>
-                    <div className="text-container">
-                        <h3>Blog post title</h3>
-                        <p>
-                            Gummi bears apple pie gummies carrot cake soufflé chocolate cake gummi bears. Powder cookie gummi bears cupcake wafer chocolate bar oat cake. Jujubes brownie candy pudding jelly beans ice cream. Brownie bear claw sweet brownie sugar plum jelly beans lemon drops candy canes. Cupcake cotton candy candy canes fruitcake cheesecake jelly-o. Liquorice wafer icing cake gingerbread tiramisu. Brownie icing cake chupa chups sesame snaps candy canes sesame snaps liquorice caramels. Bonbon tiramisu jelly marshmallow macaroon icing cheesecake. Jelly fruitcake jelly-o powder pudding.
-                        </p>
-                        <button>See more</button>
-                    </div>
+class Blog extends Component {
+  state = {
+    posts: []
+  };
 
-                </div>
-            </>
-        )
-    }
+  async componentDidMount() {
+    const response = await axios.get("http://localhost:3001/posts/");
+    let posts = response.data;
+    console.log(posts);
+
+    this.setState({ posts });
+  }
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <>
+        <div className="blog-post-container">
+          <div className="image-container"></div>
+          <div className="text-container">
+            {posts.map(post => {
+              return (
+                <>
+                  <h3>{post.title}</h3>
+                  <p>{post.body}</p>
+                  <button>See more</button>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-export default App;
+export default Blog;
