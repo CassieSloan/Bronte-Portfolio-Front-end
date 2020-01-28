1dc0e3ebfbec65fa528802d66bcc669054c18996
Cassie <cassandra.sloan16@gmail.com>
2020-01-28 13:02:30 +1100

    formatted about me page

--- src/components/pages/About_me.js
+++ src/components/pages/About_me.js
@@ -1,25 +1,32 @@
-import React, {Component} from "react";
-import "./../../styles/aboutme.scss"
+import React, { Component } from "react";
+import "./../../styles/aboutme.scss";
 
 class Aboutme extends Component {
-    render() {
-        return(
-            <>
-                <div className="flex-box">
+  render() {
+    return (
+      <>
+        <div className="flex-box">
+          <div className="image-container">
+            <div className="image"></div>
+          </div>
 
-                    <div className="image"></div> {/*maybe carousel*/}
-
-                    <div className="text-container">
-                        <p>
-                            Gummi bears apple pie gummies carrot cake soufflé chocolate cake gummi bears. Powder cookie gummi bears cupcake wafer chocolate bar oat cake. Jujubes brownie candy pudding jelly beans ice cream. Brownie bear claw sweet brownie sugar plum jelly beans lemon drops candy canes. Cupcake cotton candy candy canes fruitcake cheesecake jelly-o. Liquorice wafer icing cake gingerbread tiramisu. Brownie icing cake chupa chups sesame snaps candy canes sesame snaps liquorice caramels. Bonbon tiramisu jelly marshmallow macaroon icing cheesecake. Jelly fruitcake jelly-o powder pudding.
-                        </p>
-                    </div>
-                </div>
-                
-
-            </>
-        )
-    }
+          <div className="text-container">
+            <p>
+              Gummi bears apple pie gummies carrot cake soufflé chocolate cake
+              gummi bears. Powder cookie gummi bears cupcake wafer chocolate bar
+              oat cake. Jujubes brownie candy pudding jelly beans ice cream.
+              Brownie bear claw sweet brownie sugar plum jelly beans lemon drops
+              candy canes. Cupcake cotton candy candy canes fruitcake cheesecake
+              jelly-o. Liquorice wafer icing cake gingerbread tiramisu. Brownie
+              icing cake chupa chups sesame snaps candy canes sesame snaps
+              liquorice caramels. Bonbon tiramisu jelly marshmallow macaroon
+              icing cheesecake. Jelly fruitcake jelly-o powder pudding.
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Aboutme;
