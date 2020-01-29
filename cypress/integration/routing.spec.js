08fcd0147a7c1d9214344dd758809169c00371a4
Cassie <cassandra.sloan16@gmail.com>
2020-01-29 15:19:49 +1100

    created routing test - passed

---
+++ cypress/integration/routing.spec.js
@@ -0,0 +1,18 @@
+//loads all pages
+describe("test that all endpoints load successfully", () => {
+  it("visit the homepage", () => {
+    cy.visit("http://localhost:3000");
+  });
+  it("visit gallery", () => {
+    cy.visit("http://localhost:3000/gallery");
+  });
+  it("visit about me page", () => {
+    cy.visit("http://localhost:3000/about");
+  });
+  it("visit blog", () => {
+    cy.visit("http://localhost:3000/blog");
+  });
+  it("visit contact page", () => {
+    cy.visit("http://localhost:3000/contact");
+  });
+});
