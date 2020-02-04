907d5423196e82ae2212d9d5932bedf814ebf21b
Cassie <cassandra.sloan16@gmail.com>
2020-02-04 14:48:06 +1100

    added function to render images on GENRE page according to category

--- src/components/pages/gallery_genres/Genre.js
+++ src/components/pages/gallery_genres/Genre.js
@@ -1,38 +1,35 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom"; //method to ascertain route endpoint
 import "./../../../styles/genre.scss"; //style sheet
-import axios from "axios";
+import axios from "axios"; //use backend api
+import { setServers } from "dns";
 
 class Genre extends Component {
   state = {
     images: []
   };
 
-  //   setImageState = () => {
-  //    let path = this.props.location.pathname
-  //    console.log(this.props.location.pathname); //finds current path
-
-  //    if (path === `/gallery/${image.category}`)
-  // }
-  //function setImageState = () => {
-  // let path = this.props.location.pathname
-  //if (path === `/gallery${image.category}`(e.g "portrait")) {
-  //setState = {images: [array of portrait tagged images]}
-  //}
-  // }
-
+  //get pictures from back end and set state
   async componentDidMount() {
     try {
-      const response = await axios.get(
-        // `${process.env.REACT_APP_SERVER_URL}/images`
-        "http://localhost:3001/images"
-      );
+      const filteredImages = [];
+
+      const response = await axios.get("http://localhost:3001/images");
       let images = response.data;
       console.log(images);
+      for (let image of images) {
+        let imageCategories = image.category;
+        for (let category of imageCategories) {
+          let path = this.props.location.pathname; //finds current path
+          if (path === `/gallery/${category}`) {
+            filteredImages.push(image);
+          }
+        }
+      }
 
-      this.setState({ images });
+      this.setState({ images: filteredImages });
     } catch (error) {
-      alert(error);
+      console.log(error);
     }
   }
 
