d0e75d4d7beabb8aebfb4e2b089bcd5c31c65b0c
Cassie <cassandra.sloan16@gmail.com>
2020-02-06 13:09:53 +1100

    added more tests

--- cypress/integration/blog.spec.js
+++ cypress/integration/blog.spec.js
@@ -1,1 +1,7 @@
 //BLOG page tests
+//P A S S E D
+describe("Blog posts are rendered on page", () => {
+  it("find container pulling in blog posts", () => {
+    cy.visit("http://localhost:3000/blog").get(".blog-post-container");
+  });
+});
--- cypress/integration/gallery.spec.js
+++ cypress/integration/gallery.spec.js
@@ -1,1 +1,9 @@
 //GALLERY page tests
+describe("ensure gallery 'genre' boxes create correct endpoints in route", () => {
+  it("click each box and see url change", () => {
+    cy.visit("http://localhost:3000/gallery")
+      .get(".genre-box")
+      .click({ multiple: true })
+      .url();
+  });
+});
